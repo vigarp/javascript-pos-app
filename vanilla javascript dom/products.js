44d85a2021721b941ffd2756f2671025ca45ba22
vigarp <vigar.8421@outlook.com>
2022-02-21 09:17:09 +0700

    add vanilla js project

---
+++ vanilla javascript dom/products.js
@@ -0,0 +1,56 @@
+const menuItems = [
+    {
+        id: 0,
+        name: "Espresso",
+        price: 10000,
+        image: './img/png/jeremy-ricketts-6ZnhM-xBpos-unsplash.png',
+    },
+    {
+        id: 1,
+        name: "Cofee Latte",
+        price: 15000,
+        image: './img/png/bear.png',
+    },
+    {
+        id: 2,
+        name: "Cappucino",
+        price: 5000,
+        image: './img/png/firdaus-roslan-pN769u0KHNA-unsplash.png',
+    },
+    {
+        id: 3,
+        name: "Red Velvet Latte",
+        price: 33000,
+        image: './img/png/redvelvet.png',
+    },
+    {
+        id: 4,
+        name: "Chocorum",
+        price: 28000,
+        image: './img/png/chocorum.png',
+    },
+    {
+        id: 5,
+        name: "Black Forest",
+        price: 30000,
+        image: './img/png/blackforest.png',
+    },
+    {
+        id: 6,
+        name: "Chicken Katsu Dabu-Dabu",
+        price: 60000,
+        image: './img/png/chickenkatsu.png',
+    },
+    {
+        id: 7,
+        name: "Salmon Truffle Teriyaki",
+        price: 60000,
+        image: './img/png/salmon.png',
+    },
+    {
+        id: 8,
+        name: "Wiener Schnitzel",
+        price: 69000,
+        image: './img/png/wiener.png'
+    },
+];
